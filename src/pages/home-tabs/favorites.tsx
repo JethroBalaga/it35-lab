53855079b506ae1e131399b7d0544e9bb9854956
JethroBalaga <balagajet@gmail.com>
2025-02-17 07:39:13 +0800

    changes in favorites.tsx

---
+++ src/pages/home-tabs/favorites.tsx
@@ -0,0 +1,25 @@
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  const Favorites: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Favorites</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
