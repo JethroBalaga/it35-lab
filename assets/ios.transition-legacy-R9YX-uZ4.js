d58868777ea7412d22bfe723ce0c060795745260
JethroBalaga <balagajet@gmail.com>
2025-03-07 16:48:43 +0800

    Updates

--- assets/ios.transition-legacy-R9YX-uZ4.js
+++ assets/ios.transition-legacy-R9YX-uZ4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DNl40lZY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-Ca_iiAul.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
