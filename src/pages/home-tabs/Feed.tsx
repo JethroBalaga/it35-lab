73a48baac9f95529f9bd478d8439503658b3f315
JethroBalaga <balagajet@gmail.com>
2025-03-07 16:49:53 +0800

    changes at feed.tsx

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -54,7 +54,6 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <h1>The gays</h1>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px', padding: '10px' }}>
           {posts.map(post => (
             <IonCard key={post.id} style={{ width: '100%', maxWidth: '400px', borderRadius: '20px', overflow: 'hidden' }}>
