ffef9ce8ee689be3e5ccfca484d3547b44d834af
JethroBalaga <balagajet@gmail.com>
2025-03-24 08:31:46 +0800

    changes in .env

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
 
+
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
