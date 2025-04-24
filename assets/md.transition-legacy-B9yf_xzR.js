51e9aa3acdd11c552b92937c383eeacc61d0a85c
JethroBalaga <balagajet@gmail.com>
2025-04-24 11:48:43 +0800

    Updates

--- assets/md.transition-legacy-B9yf_xzR.js
+++ assets/md.transition-legacy-B9yf_xzR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D-xd6nWo.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BaN042HU.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DELSnzNi.js
+++ assets/swipe-back-legacy-DELSnzNi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D-xd6nWo.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BaN042HU.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
