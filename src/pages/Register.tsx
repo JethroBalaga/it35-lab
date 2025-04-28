82d945b64be2f042787ec41a1c37a1fd5e54327d
JethroBalaga <balagajet@gmail.com>
2025-04-28 13:52:41 +0800

    add register backround

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -17,6 +17,7 @@ import {
 } from '@ionic/react';
 import { supabase } from '../utils/supabaseClient';
 import bcrypt from 'bcryptjs';
+import background from '../images/vcs.gif';
 
 // Reusable Alert Component
 const AlertBox: React.FC<{ message: string; isOpen: boolean; onClose: () => void }> = ({ message, isOpen, onClose }) => {
@@ -101,6 +102,20 @@ const Register: React.FC = () => {
         <IonPage>
             <IonContent className='ion-padding'>
                 <h1>Create your account</h1>
+
+                <img
+              src={background}
+              alt="background"
+              style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%', 
+                height: '100%', 
+                objectFit: 'cover', 
+                zIndex: -1, 
+              }}
+            />
 
                 <IonInput label="Username" labelPlacement="stacked" fill="outline" type="text" placeholder="Enter a unique username" value={username} onIonChange={e => setUsername(e.detail.value!)} style={{ marginTop: '15px' }} />
                 <IonInput label="First Name" labelPlacement="stacked" fill="outline" type="text" placeholder="Enter your first name" value={firstName} onIonChange={e => setFirstName(e.detail.value!)} style={{ marginTop: '15px' }} />
