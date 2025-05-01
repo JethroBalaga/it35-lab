f8d053924363f834794e19893b143e0618c8edb6
JethroBalaga <balagajet@gmail.com>
2025-05-01 22:26:11 +0800

    fix the error on deployment

--- src/components/FeedContainer.tsx
+++ src/components/FeedContainer.tsx
@@ -279,7 +279,7 @@ const FeedContainer = () => {
       <IonPage>
         <IonHeader>
           <IonToolbar>
-            <IonTitle>Feed</IonTitle>
+            <IonTitle color="danger">Feed</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent>
